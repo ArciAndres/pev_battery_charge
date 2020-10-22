6aa4a9a282735ce2a46d9254b598e755e17dd3b7
ArciAndres <arciandres@gmail.com>
2020-10-22 03:22:23 +0200

    Initialization of environment

--- pev_battery_charge/envs/EVChargeManagement.py
+++ pev_battery_charge/envs/EVChargeManagement.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from random import random
-from ..utils.utils import createDict
+from pev_battery_charge.utils.utils import createDict
+from ray.rllib.env.multi_agent_env import MultiAgentEnv, ENV_STATE
 
 
 class PEV():
@@ -11,8 +12,7 @@ class PEV():
                        p_min=0, 
                        p_max=22,
                        soc=0,
-                       time_in=0, 
-                       time_out=180
+                       charge_time_desired=180
                        ):
         
         self.soc_max = soc_max
@@ -20,12 +20,11 @@ class PEV():
         self.p_min = p_min
         self.p_max = p_max
         self.soc = soc
-        self.time_in = time_in
-        self.time_out = time_out
+        self.charge_time_desired = charge_time_desired
         
 class ChargeStation():
     
-    def __init__(self,n_pevs_max=20, 
+    def __init__(self,n_pevs_max=50, 
                           n_pevs=0,
                           P_min=0,
                           P_max=35):
@@ -35,63 +34,97 @@ class ChargeStation():
         self.P_min = P_min
         self.P_max = P_max
 
-class BatteryCharge():
+class ChargeManagement(MultiAgentEnv):
     
-    def __init__(self, n_agents=20, 
+    def __init__(self, pevs, 
+                       charge_station,
+                       
                        interval_length=5,
-                       capacity=24,
-                       p_max=22,
-                       p_min=0, 
-                       target_charge=24,
                        total_time=960, 
-                       charge_duration_max=180,
                        charge_duration_tolerance=0.2,
                        initial_charge_max=0.5,
-                       P_max=20,
-                       seed=1515
+                       initial_charge_min=0,
+                       random_start_coeff = 1,
+                       seed=1515,
+                       
+
                  ):
         
         # This distribution of connections will put the agents across the total time, in an ordered pseudo random fashion
         
-        self.n_agents = n_agents
+        self.n_pevs = len(pevs)
+        self.pevs = pevs
+        self.charge_station = charge_station
         self.interval_length = interval_length
-        self.capacity = [capacity]*n_agents
-        self.p_max = [p_max]*n_agents
-        self.p_min = [p_min]*n_agents
-        self.target_charge = [target_charge]*n_agents
         self.total_time = total_time
-        self.charge_duration_max = charge_duration_max
         self.charge_duration_tolerance = charge_duration_tolerance 
         self.initial_charge_max = initial_charge_max
-        self.P_max = P_max
+        self.initial_charge_min = initial_charge_min
+        self.random_start_coeff = random_start_coeff
+        self.seed = seed
         
-        total_samples = int(total_time/interval_length)
-        charge_samples = charge_duration_max/interval_length
-        
-        total_samples_start = total_samples-charge_samples # Allowed start sample. Cannot start charging at very end of all, for example
-        separation = total_samples_start/n_agents
-        
-        starts, ends, socs = [], [], []
-        
-        for i in range(n_agents):
-            
-            start_time = np.floor(separation*(i+random()))
-            end_time = np.floor(start_time + charge_samples*(1-charge_duration_tolerance*(1-random())))
-            
-            #SOC can be any number between 0 and capacity
-            socs.append(round(self.capacity[i]*random()*max_initial_charge*100)/100)
-            starts.append(start_time)
-            ends.append(end_time)
-        
-        
-        
-    
+        self.total_samples = int(total_time/interval_length)
         
         
 
+                
+    def distribute_load(self):
+        
+        # charge_samples = charge_duration_max/interval_length
+        # total_samples_start = total_samples-charge_samples # Allowed start sample. Cannot start charging at very end of all, for example
+        rate, proportional_dist = self.get_shrinking_rate()
+        T_start = proportional_dist*self.total_samples
+        
+        separation = 1
+        for i , pev in enumerate(self.pevs):
+            
+            # Randomize the start time, from a point between it and the next random_start_coeff elements.
+            # random_start_coeff is 1 normally
+            if i == self.n_pevs-1:
+                pev.t_start = np.floor(T_start[i]-(T_start[i]-T_start[i-self.random_start_coeff])*random())
+                
+            else:
+                pev.t_start = np.floor(T_start[i]+(T_start[i+self.random_start_coeff]-T_start[i])*random())
+            
+            charge_samples = pev.charge_time_desired/self.interval_length
+            pev.t_end = np.floor(pev.t_start + charge_samples*(1-self.charge_duration_tolerance*(1-random())))
+            
+            # SOC can be any number between 0 and soc_max
+            pev.soc = round(pev.soc_max*random()*self.initial_charge_max*100)/100
+        
+        
+        
+    def get_proportional_initial_dist(self):
+        '''
+        Distributes the PEVs along the total charge time proportionally to their own
+        charge time. 
+        
+        Example:
+        '''
+        
+        total_charge_time_no_overlap = sum([pev.charge_time_desired for pev in self.pevs])
+        
+        proportional_dist = [pev.charge_time_desired/total_charge_time_no_overlap for pev in self.pevs]        
+        return np.add.accumulate(proportional_dist)-proportional_dist[0]
+        
+    def get_shrinking_rate(self):
+        '''
+        Shrinkes the proportional distribution so that all desired end times of desired
+        charge are inside the Total charge time.
+        '''
+        dist = self.get_proportional_initial_dist()
+        
+        end_times = []
+        for start, pev in zip(dist, self.pevs):
+            end_time = start*self.total_time + pev.charge_time_desired
+            end_times.append(end_time)
+            
+        if max(end_times) > self.total_time:            
+            shrink_rate = self.total_time/max(end_times)
+            
+        else:
+            shrink_rate = 1 # no need to shrink
+        
+        return shrink_rate, dist*shrink_rate
     
-        
-        
-        
-        
-        
+   # def plot
