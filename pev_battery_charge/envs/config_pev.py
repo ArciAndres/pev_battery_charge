478869c6973c37e7d593d32bb3ab0a38e4b477c9
ArciAndres <arciandres@gmail.com>
2021-01-09 10:19:31 +0100

    Scenario name

--- pev_battery_charge/envs/config_pev.py
+++ pev_battery_charge/envs/config_pev.py
@@ -17,7 +17,7 @@ def get_config(notebook=False):
     
     # env
     parser.add_argument("--env_name", type=str, default='BatteryCharge')
-    parser.add_argument("--env_name", type=str, default='PEVChargeSchedule')
+    parser.add_argument("--scenario_name", type=str, default='PEVChargeSchedule')
     parser.add_argument("--num_agents", type=int, default=20, help='Number of PEVs to schedule during training')
     parser.add_argument("--share_reward", action='store_false', default=False)
     
